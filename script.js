da0fa5169ac375b18e308f9c61b621c04d83e63a
vivrehal <dimanvivek777@gmail.com>
2023-10-20 00:41:06 +0530

    Added Delete and Update Blog Functionality also added render blog content in a new page

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function getQueryParam(param) {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    return urlSearchParams.get(param);
+}
+
+function getBlogById(blogId) {
+    const blogs = JSON.parse(localStorage.getItem('blogs')) || [];
+    return blogs.find(blog => blog.id === parseInt(blogId));
+}
+
+
 if(window.location.href.includes('blog.html'))
 {
     const quill = new Quill('#editor', {
@@ -26,16 +37,27 @@ if(window.location.href.includes('blog.html'))
         placeholder: 'Write your blog here...',
     });
 
+    const oldblogId = getQueryParam('id');
+    const oldblog = getBlogById(oldblogId);
+    if(oldblog){
+        document.getElementById("cover")
+        quill.root.innerHTML=oldblog.content; 
+    }
+
 
 
 function saveBlog(){
-    const title = quill.getText(0, 50); 
+    if(oldblog){
+        deleteBlog(oldblog.id)
+    }
+    const title = document.getElementById("title").value; 
+    console.log(title)
     const content = quill.root.innerHTML; 
     const cover=document.getElementById("cover");
     const reader = new FileReader();
     const img={url:"https://images.pexels.com/photos/18786198/pexels-photo-18786198/free-photo-of-a-ladder-is-leaning-against-a-wall-in-the-sun.jpeg"};
     let blogs = JSON.parse(localStorage.getItem("blogs")) || [];
-    const blog = { title, content, ...img};
+    const blog = {id:Date.now(), title, content, ...img};
     blogs.push(blog);
     localStorage.setItem("blogs", JSON.stringify(blogs));
     if(cover.files){
@@ -50,7 +72,7 @@ function saveBlog(){
     console.log(img.url)
 
 
-    // window.location.href = "index.html";
+    window.location.href = "index.html";
 }
 }
 else if (window.location.href.includes('index.html')){
@@ -60,11 +82,80 @@ else if (window.location.href.includes('index.html')){
         blogsContainer.innerHTML = "";
         blogs.forEach(blog => {
             const blogDiv = document.createElement("div");
-            blogDiv.className="blogCards"
-            blogDiv.innerHTML = `<h2>${blog.title}</h2><img src=${blog.url}><p>${blog.content}</p>`;
+            const btnsDiv = document.createElement("div");
+            blogDiv.className = "blogCards";
+            btnsDiv.className = "blogCardsbtns";
+    
+            // Create a clickable link for each blog card
+            const blogLink = document.createElement("a");
+            blogLink.href = `view.html?id=${blog.id}`;
+            blogLink.innerHTML = `
+                <h2 class="cardTitle">${blog.title.toUpperCase()}</h2>
+                <img src=${blog.url}>
+            `;
+            btnsDiv.innerHTML=`
+            <button class="updateBtn" onclick="updateBlog(${blog.id})">Update Blog</button>
+            <button class="deleteBtn" onclick="deleteBlog(${blog.id})">Delete Blog</button>
+            `;
+
+    
+            // Append the clickable link and buttons to the blog card container
+            blogDiv.appendChild(blogLink);
+            blogDiv.appendChild(btnsDiv);
+            
+            // Append the blog card to the container
             blogsContainer.appendChild(blogDiv);
         });
     }
+   
     
     displayBlogs();
+}
+
+else if(window.location.href.includes('view.html')){
+    const blogId = getQueryParam('id');
+    const blog = getBlogById(blogId);
+
+    const blogTitle = document.getElementById('blogTitle');
+    const blogImage = document.getElementById('blogImage');
+    const blogContent = document.getElementById('blogContent');
+
+    if (blog) {
+        blogTitle.textContent = blog.title;
+        blogImage.src = blog.url;
+        blogContent.innerHTML = blog.content;
+    } else {
+        // Handle blog not found
+        window.location.href = 'index.html';
+    }
+
+    
+}
+
+
+function deleteBlog(blogId) {
+    // Get blogs from local storage
+    let blogs = JSON.parse(localStorage.getItem("blogs")) || [];
+
+    // Find the index of the blog with the given ID
+    const blogIndex = blogs.findIndex(blog => blog.id === blogId);
+
+    if (blogIndex !== -1) {
+        // Remove the blog from the array
+        blogs.splice(blogIndex, 1);
+
+        // Update local storage with the modified blogs array
+        localStorage.setItem("blogs", JSON.stringify(blogs));
+
+        // Reload the page to reflect the changes
+        window.location.reload();
+    } else {
+        console.error("Blog not found.");
+    }
+}
+
+function updateBlog(blogId) {
+    window.location.href = `blog.html?id=${blogId}`;
+    // Redirect the user to blog.html with the specific blogId in the URL for editing
+
 }
