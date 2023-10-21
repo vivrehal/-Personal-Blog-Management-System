10db37a01280b117ebb3d9945d5a5a06822e1e61
vivrehal <dimanvivek777@gmail.com>
2023-10-21 17:05:56 +0530

    Added active flag in classname of toolbar btns for styling

--- JS/editor.js
+++ JS/editor.js
@@ -7,16 +7,34 @@ const editor = document.getElementById("editor");
 //functions
 
 italicBtn.addEventListener("click", () => {
+    if(italicBtn.classList.contains("activeItalic")){
+        italicBtn.classList.remove("activeItalic")
+    }
+    else{
+        italicBtn.classList.add("activeItalic")
+    }
     document.execCommand("italic", false, null);
     editor.focus();
 });
 
 boldBtn.addEventListener("click", () => {
+    if(boldBtn.classList.contains("activeBold")){
+        boldBtn.classList.remove("activeBold")
+    }
+    else{
+        boldBtn.classList.add("activeBold")
+    }
     document.execCommand("bold", false, null);
     editor.focus();
 });
 
 underlineBtn.addEventListener("click", () => {
+    if(underlineBtn.classList.contains("activeUnderline")){
+        underlineBtn.classList.remove("activeUnderline")
+    }
+    else{
+        underlineBtn.classList.add("activeUnderline")
+    }
     document.execCommand("underline", false, null);
     editor.focus();
 });
